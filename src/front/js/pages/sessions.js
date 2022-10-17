7fd73095c82db98f2c4437caad1585272d05d9c6
Jose Morrone <josemmorrone@gmail.com>
2022-10-17 12:31:50 -0400

    some changes

--- src/front/js/pages/sessions.js
+++ src/front/js/pages/sessions.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { JitsiMeeting } from '@jitsi/react-sdk';
+import { Redirect } from 'react-router-dom';
 
 const Sessions = () => {
     return (
         <div>
             <JitsiMeeting
+                interfaceConfigOverwrite={{
+                    DEFAULT_BACKGROUND: '#040404',
+                    DEFAULT_WELCOME_PAGE_LOGO_URL: '',
+                    JITSI_WATERMARK_LINK: '',
+                    SHOW_JITSI_WATERMARK: false,
+                }}
                 configOverwrite={{
                     startWithAudioMuted: true,
                     startWithVideoMuted: true
@@ -14,6 +21,9 @@ const Sessions = () => {
                     node.style.height = '90vh';
                     node.style.marginLeft = "250px";
                 }}
+                onReadyToClose={() => { return <Redirect to="/calendar" /> }
+                }
+                onApiReady={() => console.log("hola")}
             />
         </div>
     );
